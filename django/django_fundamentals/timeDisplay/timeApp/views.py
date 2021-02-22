20fd5495f8344baa7ec3ce67a458654c34c87713
Edgar Diaz-Gutierrez <ediaz-gutierrez@hotmail.com>
2021-02-22 11:25:58 -0800

    submitting timeDisplay assignment

---
+++ django/django_fundamentals/timeDisplay/timeApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+# from time import gmtime, strftime
+from datetime import datetime
+from pytz import timezone
+import pytz
+
+# # Create your views here.
+# def root(request):
+#     context = {
+#         "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+#     }
+#     return render(request,'root.html', context)
+
+def root(request):
+    #date format
+    date_format='%m/%d/%Y %H:%M:%S %Z'
+    #setting time to now and TimeZone to UTC
+    date = datetime.now(tz=pytz.utc)
+    #setting TZ to US PACIFIC
+    date = date.astimezone(timezone('US/Pacific'))
+    # displaying time
+    myTime = date.strftime(date_format)
+    context = {
+        "time": myTime
+    }
+    return render(request, "root.html", context)
