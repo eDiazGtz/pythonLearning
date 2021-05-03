e17109832cb018ed48dde7137b817b7598246fa3
Edgar Diaz-Gutierrez <ediaz-gutierrez@hotmail.com>
2021-05-03 09:33:47 -0700

    this should be a conflict

--- django/django_fullstack/djangoForms/djangoFormsApp/urls.py
+++ django/django_fullstack/djangoForms/djangoFormsApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', landing, name='landing'),
     path('register', register, name='register'),
     path('login', do_login, name='login'), #changed the name of LOGIN for django Login method in views.py
-    path('dashboard', dashboard, name='dashboard'),
+    path('dashboard', dashboard, name='dashboard'), #added another comment
     path('logout', logout, name='logout'),
     path('dragon/create', createDragon),
     
