9485945714566f8f097c1169fcf703a08e49f288
Edgar Diaz-Gutierrez <ediaz-gutierrez@hotmail.com>
2021-05-03 10:20:00 -0700

    fixing error

--- django/django_fullstack/djangoForms/djangoFormsApp/urls.py
+++ django/django_fullstack/djangoForms/djangoFormsApp/urls.py
@@ -4,7 +4,7 @@ from .views import landing, dashboard, register, do_login, logout, createDragon
 urlpatterns = [
     path('', landing, name='landing'),
     path('register', register, name='register'),
-    path('login', do_login, name='login'), #I want a comment now
+    path('login', do_login, name='login'),
     path('dashboard', dashboard, name='dashboard'),
     path('logout', logout, name='logout'),
     path('dragon/create', createDragon),
